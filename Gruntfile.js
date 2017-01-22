9bf76993d66e21b8d2fe285878761df0ab33c701
Pankaj Mondal <pankajgurudeb@gmail.com>
2017-01-22 13:26:44 +0530

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+        	all: ['src/**/*.js'],
+        	options: {
+        		globals: {
+        			_: false,
+        			$: false
+        		},
+        		browser: true,
+        		devel: true
+        	}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+};
